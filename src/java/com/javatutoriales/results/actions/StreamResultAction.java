631022a84e76d53e405268609ac47f2a68805a3a
InmaGs <inmac.garciasanchez@gmail.com>
2014-07-29 11:21:25 +0200

    Añadida clase

--- src/java/com/javatutoriales/results/actions/StreamResultAction.java
+++ src/java/com/javatutoriales/results/actions/StreamResultAction.java
@@ -7,6 +7,9 @@
 package com.javatutoriales.results.actions;
 
 import com.opensymphony.xwork2.ActionSupport;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 /**
  *
@@ -14,4 +17,40 @@ import com.opensymphony.xwork2.ActionSupport;
  */
 public class StreamResultAction extends ActionSupport {
     
+    //Declaramos las variables que vamos a usar:
+    
+    private int bytesArchivo;
+    private InputStream streamImagen;
+    private String nombreImagen;
+    
+    //Declaramos métodos getter que proporcionan los valores de las variables
+    //para que luego puedan leerse en el action:
+    public int getBytesArchivo(){
+        return bytesArchivo;
+    }
+    
+    public InputStream getStreamImagen(){
+        return streamImagen;
+    }
+    
+    public String getNombreImagen(){
+        return nombreImagen;
+    }
+    
+    //Sobre-escribimos método execute. Dentro se crea un objeto de tipo File que
+    //apuntará al archivo de la imagen. A través de este objeto obtendremos el tamaño
+    //y su nombre.
+    @Override
+    public String execute() throws Exception{
+        
+        File archivoImagen = new File("C:\\Users\\inmaculada.garcia\\Pictures\\homer.png");
+        bytesArchivo = (int)archivoImagen.length();
+        nombreImagen = archivoImagen.getName();
+        
+        //Creamos InputStream para el flujo de bytes de la imagen
+        
+        streamImagen = new FileInputStream(archivoImagen);
+        
+        return SUCCESS;
+    }
 }
